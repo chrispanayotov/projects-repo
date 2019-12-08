7e6160ccf54ab0034126f5e920e74c1b181ad274
UrielX <panayotov.christian@gmail.com>
2019-12-08 02:09:52 +0200

    updated some modules

--- nand2tetris/projects/Assembler/assembler.py
+++ nand2tetris/projects/Assembler/assembler.py
@@ -1,2 +1,19 @@
-import parser, symbol_table, translator
+from parser import Parser
 
+# new_file = Parser('/home/uriel/projects-repo/nand2tetris/projects/Assembler/Add.asm')
+
+assembly_program = Parser('/home/uriel/projects-repo/nand2tetris/projects/Assembler/Add.asm')
+
+while True:
+
+    current_command = assembly_program.advance()
+    command_type = assembly_program.command_type(current_command)
+    
+
+    if command_type == 'A':
+        a_command_bin = assembly_program.symbol_or_decimal(current_command)
+        print(a_command_bin)
+    elif command_type == 'L':
+        pass
+    else:   # Command is C
+        pass
--- nand2tetris/projects/Assembler/translator.py
+++ nand2tetris/projects/Assembler/translator.py
@@ -2,6 +2,15 @@
 # and translates them into binary code.
 # Translator contains 3 methods - comp(7bits), dest(3bits) and jump(3bits).
 
+# Examples:
+# Command -> Binary representation
+# @2 -> 
+# D=M ->
+# 0;JMP ->
+
+
+from reference_table import comp_dict_A0, comp_dict_A1, dest_dict, jump_dict
+
 class Translator:
     def __init__(self, dest, comp, jump):
         self.dest = dest
