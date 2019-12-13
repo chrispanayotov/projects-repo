a0ea31b8dd461b4c8eee7195af625a7a2e1fb081
UrielX <panayotov.christian@gmail.com>
2019-12-13 20:08:29 +0200

    fixed is_parsed(), is_decimal()  methods, added remove_labels() method

--- nand2tetris/projects/Assembler/parser.py
+++ nand2tetris/projects/Assembler/parser.py
@@ -4,8 +4,6 @@
 # to the command's components (fields and symbols)
 # Also, it detects and removes whitespaces and comments
 
-
-from translator import convert_to_bin
 from shutil import copyfile
 
 class Parser:
@@ -13,7 +11,6 @@ class Parser:
         self.source_file = source_file              # Loads the source file
         self.assembly_program = self.open_file()   # Removes comments and whitespaces
         self.assembly_program_len = len(self.assembly_program.split('\n'))   # Gets lenght of program
-        self.is_parsed = self.is_parsed()           # Checks whether the program is parsed
 
     line_counter = 0
 
@@ -34,21 +31,30 @@ class Parser:
         return cleaned_assembler_program.strip()
 
     
+    def remove_labels(self):
+        assembly_program_no_labels = ''
+
+        for index, line in enumerate(self.assembly_program.splitlines()):
+            if line.startswith('('):
+                print(index)
+                del self.assembly_program.splitlines()[index]
+                
+        
+        print(self.assembly_program.splitlines())
+
+    
     def is_parsed(self):
         '''Method stores the number of lines of the assembly program.
         Returns True when the program is parsed, else advances to the next line'''
 
-        if self.assembly_program_len != Parser.line_counter:
-            return False
-
-        return True
+        return self.assembly_program_len == self.line_counter
 
 
     def advance(self):
         '''Method reads the next command from the assembly program and makes it
         the current command. Should only be called while self.is_parsed is False'''
 
-        if self.line_counter > int(self.assembly_program_len):
+        if self.line_counter >= self.assembly_program_len:
             self.is_parsed()
 
         # Select command
@@ -73,30 +79,19 @@ class Parser:
             return 'C_COMMAND'
 
 
-    def symbol_or_decimal(self, command):
-        '''Method returns the symbol or decimal Xxx of the current command @xxx or (XXX).
-        Should only be called if self.command_type() is A_COMMAND or L_COMMAND.
-        Converts decimal to binary and returns a string or stores symbol in symbol_table'''
+    def is_decimal(self, command):
+        '''Method returns True when the current A_Command is a decimal number.
+        Otherwise, returns False, which means that the command is a symbol and 
+        needs to be added to SymbolTable
+        Should only be called if self.command_type() is A_COMMAND.'''
 
-        def check_for_integer(string):
-            # Function checks if the A_COMMAND is a symbol or a decimal number
-            try:
-                int(string)
-                return True
-            except:
-                return False
+        # Check if the A_COMMAND is a symbol or decimal and return num or label.
+        try:
+            int(command[1:])
+            return True
+        except:
+            return False
         
-        # Remove the '@' symbol
-        a_command = command[1:]
-        # Check if the A_COMMAND is a symbol or decimal. 
-        is_decimal = check_for_integer(a_command)
-
-        if is_decimal == True:
-            # Converts decimal number to 16-bit binary representation
-            return convert_to_bin(a_command)
-        else:
-            pass # Else its a symbol and needs to be added to symbol_table.py
-
 
     def dest_mnemonics(self, command):
         '''Returns the dest mnemonic (string) in the current C-command (8 possibilities).
@@ -128,10 +123,3 @@ class Parser:
     #     '''Method creates a new file with comments and whitespaces removed'''
     #     with open('/home/uriel/projects-repo/nand2tetris/projects/Assembler/Clean.asm', 'w') as f:
     #         return f.write(open_file)
-
-
-
-# x = parse_file.open_file
-
-# for line in x.splitlines():
-#     print(line)
