2478c3c3db1a72c7a8038801865f60899c77ac2d
UrielX <panayotov.christian@gmail.com>
2019-12-13 20:14:03 +0200

    added Symbol Table module

--- nand2tetris/projects/Assembler/symbol_table.py
+++ nand2tetris/projects/Assembler/symbol_table.py
@@ -1,1 +1,25 @@
 # Module keeps correspondence between symbolic labels and numeric addresses
+
+from reference_tables import built_ins_dict
+
+class SymbolTable:
+    def __init__(self):
+        self.symbol_table = {}
+        self.add_builtins = self.symbol_table.update(built_ins_dict)
+        
+
+    def add_entry(self, symbol: str, address: int):
+        '''Method adds the pair (symbol, address) to the table.'''
+        self.symbol_table[symbol] = address
+
+
+    def contains(self, symbol: str):
+        '''Method checks if the symbol table contains the given symbol.
+        Returns Boolean'''
+        return symbol in self.symbol_table.keys()
+
+
+    def get_address(self, symbol: str):
+        '''Method returns the numeric address associated with the symbol.
+        Returns int'''
+        return self.symbol_table[symbol]
